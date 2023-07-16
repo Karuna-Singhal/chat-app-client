43c6a2c39b434679e23a0458b1310c1740e4f60b
Karuna <singhalkaruna73@gmail.com>
2023-07-16 23:49:08 +0530

    Contact and welcome page done

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,5 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { allUsersRoute } from "../utils/APIRoutes";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
 
 export default function Chat() {
-  return <div>Chat</div>;
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [currentChat, setCurrentChat] = useState(null);
+
+  useEffect(() => {
+    if (!localStorage.getItem("chat-app-user")) {
+      navigate("/login");
+    } else {
+      setCurrentUser(JSON.parse(localStorage.getItem("chat-app-user")));
+    }
+  }, []);
+
+  useEffect(() => {
+    async function CurrentUserChat() {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+          setContacts(data.data);
+        } else {
+          navigate("/setAvatar");
+        }
+      }
+    }
+
+    CurrentUserChat();
+  }, [currentUser]);
+
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
+
+  return (
+    <>
+      <Container>
+        <div className="container">
+          <Contacts
+            contacts={contacts}
+            currentuser={currentUser}
+            chatChange={handleChatChange}
+          />
+          <Welcome currentUser={currentUser} />
+        </div>
+      </Container>
+    </>
+  );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -2,3 +2,4 @@ const localhost = "http://localhost:5000";
 export const registerRoute = `${localhost}/api/auth/register`;
 export const loginRoute = `${localhost}/api/auth/login`;
 export const setAvatarRoute = `${localhost}/api/auth/setAvatar`;
+export const allUsersRoute = `${localhost}/api/auth/allusers`;
