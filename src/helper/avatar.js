1afa4222f5e1b279ad2926151af3852ea55f9694
Karuna <singhalkaruna73@gmail.com>
2023-07-16 11:53:14 +0530

    avatar page done

---
+++ src/helper/avatar.js
@@ -0,0 +1,6 @@
+export const avatars = [
+  "/avataaars-1.svg",
+  "/avataaars-2.svg",
+  "/avataaars-3.svg",
+  "/avataaars-4.svg",
+];
--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,3 +1,4 @@
 const localhost = "http://localhost:5000";
 export const registerRoute = `${localhost}/api/auth/register`;
 export const loginRoute = `${localhost}/api/auth/login`;
+export const setAvatarRoute = `${localhost}/api/auth/setAvatar`;
